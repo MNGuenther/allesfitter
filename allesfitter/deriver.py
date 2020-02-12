bug fixes in deriver.py; unit conversion for surface gravity calculation, and updated computation for transit depth

allesfitter/deriver.py
@@ -122,9 +122,7 @@ def derive(samples, mode):
         derived_samples[companion+'_R_companion/a'] = get_params(companion+'_rsuma') * get_params(companion+'_rr') / (1. + get_params(companion+'_rr'))
         derived_samples[companion+'_R_companion_(R_earth)'] = star['R_star'] * get_params(companion+'_rr') * R_sun.value / R_earth.value #in R_earth
         derived_samples[companion+'_R_companion_(R_jup)'] = star['R_star'] * get_params(companion+'_rr') * R_sun.value / R_jup.value #in R_jup
-
-        derived_samples[companion+'_depth_undiluted'] = 1e3*get_params(companion+'_rr')**2 #in ppt
-
+        
 
         #::: orbit
         derived_samples[companion+'_a_(R_sun)'] = star['R_star'] / derived_samples[companion+'_R_star/a']   
@@ -137,6 +135,7 @@ def derive(samples, mode):
         if np.isnan(derived_samples[companion+'_w']).all():
             derived_samples[companion+'_w'] = 0.
         
+        
         #::: mass
         if companion+'_K' in config.BASEMENT.params:
             a_1 = 0.019771142 * get_params(companion+'_K') * get_params(companion+'_period') * np.sqrt(1. - derived_samples[companion+'_e']**2)/sin_d(derived_samples[companion+'_i'])
@@ -146,14 +145,6 @@ def derive(samples, mode):
             derived_samples[companion+'_M_companion_(M_jup)'] = derived_samples[companion+'_q'] * star['M_star'] * M_sun.value / M_jup.value #in M_jup
         else:
             derived_samples[companion+'_M_companion'] = None
-            
-            
-        #::: surface gravity
-        #::: from Southworth+ 2007
-        if companion+'_K' in config.BASEMENT.params:
-            derived_samples[companion+'_g'] = 2*np.pi / get_params(companion+'_period') * (1.-derived_samples[companion+'_e']**2)**0.5 * get_params(companion+'_K') / (derived_samples[companion+'_R_companion/a']**2 * sin_d(derived_samples[companion+'_i']))
-        else:
-            derived_samples[companion+'_g'] = None        
             
             
         #::: time of occultation    
@@ -171,6 +162,7 @@ def derive(samples, mode):
         else:
             derived_samples[companion+'_b_occ'] = None
         
+        
         #::: transit duration 
         derived_samples[companion+'_T_tra_tot'] = get_params(companion+'_period')/np.pi *24.  \
                                   * np.arcsin( derived_samples[companion+'_R_star/a'] \
@@ -182,39 +174,61 @@ def derive(samples, mode):
                                              / sin_d(derived_samples[companion+'_i']) ) #in h
                                   
 
-        #::: secondary eclipse / occultation depth (per inst)
+        #::: primary and secondary eclipse depths (per inst) / transit and occultation depths (per inst)
         for inst in config.BASEMENT.settings['inst_phot']:
             
             N_less_samples = 1000
             
+            derived_samples[companion+'_depth_tr_diluted_'+inst] = np.zeros(N_less_samples)*np.nan #1e3*get_params(companion+'_rr')**2 #in ppt
             derived_samples[companion+'_depth_occ_diluted_'+inst]  = np.zeros(N_less_samples)*np.nan
             derived_samples[companion+'_ampl_ellipsoidal_diluted_'+inst]  = np.zeros(N_less_samples)*np.nan
             derived_samples[companion+'_ampl_sbratio_diluted_'+inst] = np.zeros(N_less_samples)*np.nan
             derived_samples[companion+'_ampl_geom_albedo_diluted_'+inst] = np.zeros(N_less_samples)*np.nan
             derived_samples[companion+'_ampl_gdc_diluted_'+inst] = np.zeros(N_less_samples)*np.nan
             
-            if config.BASEMENT.settings['secondary_eclipse'] is True:
+            
+            #::: iterate through all samples, draw different models and measure the depths
+            logprint('Deriving primary and secondary eclipse depths / transit and occultation depths from model curves...')
+            for i in tqdm( range(N_less_samples) ):
                 
-                def plottle(fname, title, i):
-                    if i==0:
-                        fig = plt.figure()
-                        plt.plot(xx,model,'b.')
-                        plt.title(title)
-                        fig.savefig(os.path.join(config.BASEMENT.outdir,fname), bbox_inches='tight')
-                        plt.close(fig)
+                #::: setting the model params and time axes
+                ii = np.random.randint(low=0,high=samples.shape[0])
+                s = samples[ii,:]
+                p = update_params(s)
+                xx0 = p[companion+'_epoch'] + np.arange(-0.25*p[companion+'_period'], 0.75*p[companion+'_period'], 1./24./60.) #one full orbit without the transit (in time units), in 1 min steps
+                ind_tr, ind_out = index_transits(xx0, p[companion+'_epoch'], p[companion+'_period'], np.median(derived_samples[companion+'_T_tra_tot'])/24.) #find the primary eclipse / transit
+            
+            
+                #:: calculating primary eclipse / transit depth
+                xx = xx0[ind_tr]
+                model = calculate_model(p, inst, 'flux', xx=xx) #evaluated on xx (!)
+                derived_samples[companion+'_depth_tr_diluted_'+inst][i] = ( 1. - np.min(model) ) * 1e3 #in ppt
+        
+        
+                #:: calculating secondary eclipse / occultation depth
+                if config.BASEMENT.settings['secondary_eclipse'] is True:
+                    xx = xx0[ind_out]
+
+                    def plottle(fname, title, model):
+                        if i==0:
+                            fig = plt.figure()
+                            plt.plot(xx,model,'b.')
+                            plt.title(title)
+                            fig.savefig(os.path.join(config.BASEMENT.outdir,fname), bbox_inches='tight')
+                            plt.close(fig)
                     
-                logprint('Deriving occultation depths from model curves...')
-                for i in tqdm( range(N_less_samples) ):
-                    ii = np.random.randint(low=0,high=samples.shape[0])
-                    s = samples[ii,:]
-                    p = update_params(s)
-                    xx = p[companion+'_epoch'] + np.arange(-0.25*p[companion+'_period'], 0.75*p[companion+'_period'], 1./24./60.) #one full orbit without the transit (in time units), in 1 min steps
-                    ind_tr, ind_out = index_transits(xx, p[companion+'_epoch'], p[companion+'_period'], np.median(derived_samples[companion+'_T_tra_tot'])/24.) #ignore the secondary eclipse here
-                    xx = xx[ind_out]
+                # logprint('Deriving occultation depths from model curves...')
+                # for i in tqdm( range(N_less_samples) ):
+                #     ii = np.random.randint(low=0,high=samples.shape[0])
+                #     s = samples[ii,:]
+                #     p = update_params(s)
+                #     xx = p[companion+'_epoch'] + np.arange(-0.25*p[companion+'_period'], 0.75*p[companion+'_period'], 1./24./60.) #one full orbit without the transit (in time units), in 1 min steps
+                #     ind_tr, ind_out = index_transits(xx, p[companion+'_epoch'], p[companion+'_period'], np.median(derived_samples[companion+'_T_tra_tot'])/24.) #ignore the secondary eclipse here
+                #     xx = xx[ind_out]
                     
                     #::: occultation depth (very simplistic; only ok for exoplanets, not binaries)
                     model = calculate_model(p, inst, 'flux', xx=xx) #evaluated on xx (!)
-                    plottle('phase_curve_occultation_depth.pdf', 'occultation depth', i)
+                    if i==0: plottle('phase_curve_occultation_depth.pdf', 'occultation depth', model)
                     derived_samples[companion+'_depth_occ_diluted_'+inst][i] = ( np.max(model) - np.min(model) ) * 1e6 #in ppm
 
 
@@ -287,6 +301,7 @@ def derive(samples, mode):
                     
                     
             #resize the arrays to match the true N_samples (by redrawing the 1000 values)
+            derived_samples[companion+'_depth_tr_diluted_'+inst]          = np.resize(derived_samples[companion+'_depth_tr_diluted_'+inst], N_samples)
             derived_samples[companion+'_depth_occ_diluted_'+inst]         = np.resize(derived_samples[companion+'_depth_occ_diluted_'+inst], N_samples)
             derived_samples[companion+'_ampl_ellipsoidal_diluted_'+inst]  = np.resize(derived_samples[companion+'_ampl_ellipsoidal_diluted_'+inst], N_samples)
             derived_samples[companion+'_ampl_sbratio_diluted_'+inst]      = np.resize(derived_samples[companion+'_ampl_sbratio_diluted_'+inst], N_samples)
@@ -294,15 +309,14 @@ def derive(samples, mode):
             derived_samples[companion+'_ampl_gdc_diluted_'+inst]          = np.resize(derived_samples[companion+'_ampl_gdc_diluted_'+inst], N_samples)
 
     
-        #::: undiluted (not per companions; per inst)
+        #::: undiluted (per companion; per inst)
         for inst in config.BASEMENT.settings['inst_phot']:
             dil = get_params('light_3_'+inst)
             if np.isnan(dil):
                 dil = 0
         #        if np.mean(dil)<0.5: dil = 1-dil
         
-            derived_samples[companion+'_depth_diluted_'+inst] = derived_samples[companion+'_depth_undiluted'] * (1. - dil) #in ppt
-            
+            derived_samples[companion+'_depth_tr_undiluted_'+inst] = derived_samples[companion+'_depth_tr_diluted_'+inst] / (1. - dil) #in ppt
             derived_samples[companion+'_depth_occ_undiluted_'+inst] = derived_samples[companion+'_depth_occ_diluted_'+inst] / (1. - dil) #in ppm
             derived_samples[companion+'_ampl_ellipsoidal_undiluted_'+inst] = derived_samples[companion+'_ampl_ellipsoidal_diluted_'+inst] / (1. - dil) #in ppm
             derived_samples[companion+'_ampl_sbratio_undiluted_'+inst] = derived_samples[companion+'_ampl_sbratio_diluted_'+inst] / (1. - dil) #in ppm
@@ -321,11 +335,11 @@ def derive(samples, mode):
         derived_samples[companion+'_host_density'] = 3. * np.pi * (1./derived_samples[companion+'_R_star/a'])**3. / (get_params(companion+'_period')*86400.)**2 / 6.67408e-8 #in cgs
   
     
-        #::: stellar surface gravity (via Southworth+ 2007)
+        #::: the companion's surface gravity (individual posterior distribution for each companion; via Southworth+ 2007)
         try:
-            derived_samples[companion+'_host_surface_gravity'] = 2. * np.pi / (get_params(companion+'_period')*86400.) * np.sqrt((1.-derived_samples[companion+'_e']**2)) * get_params(companion+'_K') / (derived_samples[companion+'_R_companion/a'])**2 / sin_d(derived_samples[companion+'_i'])
+            derived_samples[companion+'_surface_gravity'] = 2. * np.pi / (get_params(companion+'_period')*86400.) * np.sqrt((1.-derived_samples[companion+'_e']**2)) * (get_params(companion+'_K')*1e5) / (derived_samples[companion+'_R_companion/a'])**2 / sin_d(derived_samples[companion+'_i'])
         except:
-            derived_samples[companion+'_host_surface_gravity'] = 0.
+            derived_samples[companion+'_surface_gravity'] = 0.
         
         
         #::: period ratios (for ressonance studies)
@@ -420,22 +434,22 @@ def derive(samples, mode):
         names.append( companion+'_host_density' )
         labels.append( '$rho_\mathrm{\star;'+companion+'}$ (cgs)' )
     
-        names.append( companion+'_host_surface_gravity')
-        labels.append( '$\log{g_\mathrm{\star;'+companion+'}}$ (cgs)' )
+        names.append( companion+'_surface_gravity')
+        labels.append( '$g_\mathrm{\star;'+companion+'}$ (cgs)' )
         
         names.append( companion+'_Teq' )
         labels.append( '$T_\mathrm{eq;'+companion+'}$ (K)' )
         
-        names.append( companion+'_depth_undiluted' )
-        labels.append( '$\delta_\mathrm{undil; '+companion+'}$ (ppt)' )
-        
-        names.append( companion+'_depth_occ_undiluted' )
-        labels.append( '$\delta_\mathrm{occ; undil; '+companion+'}$ (ppm)' )
-        
         for inst in config.BASEMENT.settings['inst_phot']:
             
-            names.append( companion+'_depth_diluted_'+inst )
-            labels.append( '$\delta_\mathrm{dil; '+companion+'; '+inst+'}$ (ppt)' )
+            names.append( companion+'_depth_tr_undiluted_'+inst )
+            labels.append( '$\delta_\mathrm{tr; undil; '+companion+'; '+inst+'}$ (ppt)' )
+            
+            names.append( companion+'_depth_tr_diluted_'+inst )
+            labels.append( '$\delta_\mathrm{tr; dil; '+companion+'; '+inst+'}$ (ppt)' )
+        
+            names.append( companion+'_depth_occ_undiluted_'+inst )
+            labels.append( '$\delta_\mathrm{occ; undil; '+companion+'; '+inst+'}$ (ppm)' )
             
             names.append( companion+'_depth_occ_diluted_'+inst )
             labels.append( '$\delta_\mathrm{occ; dil; '+companion+'; '+inst+'}$ (ppm)' )

allesfitter/mcmc_output.py
@@ -24,7 +24,8 @@ sns.set_context(rc={'lines.markeredgewidth': 1})
 
 #::: modules
 import numpy as np
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt    
+from matplotlib.ticker import ScalarFormatter
 import os
 from shutil import copyfile
 import emcee
@@ -159,21 +160,20 @@ def plot_MCMC_corner(sampler):
             params_median2[companion+'_epoch'] -= int(params_median[companion+'_epoch'])
                 
     for i,l in enumerate(labels):
-        if units[i]!='':
+        if len( units[i].strip(' ') ) > 0:
             labels[i] = str(labels[i]+' ('+units[i]+')')
-        
         
     #::: corner plot
     fig = corner(samples, 
                  labels = labels,
                  range = [0.999]*config.BASEMENT.ndim,
                  quantiles=[0.15865, 0.5, 0.84135],
-                 show_titles=False, title_kwargs={"fontsize": 14},
-#                 label_kwargs={"fontsize": 20},
+                 show_titles=False, 
+                 #title_kwargs={"fontsize": 14},
+                 label_kwargs={"fontsize":32, "rotation":45, "horizontalalignment":'right'},
                  max_n_ticks=3,
                  truths=config.BASEMENT.fittruths)
     caxes = np.reshape(np.array(fig.axes), (config.BASEMENT.ndim,config.BASEMENT.ndim))
-    
 
     #::: set allesfitter titles
     for i, key in enumerate(config.BASEMENT.fitkeys): 
@@ -181,16 +181,27 @@ def plot_MCMC_corner(sampler):
         value = round_tex(params_median2[key], params_ll2[key], params_ul2[key])
         ctitle = r'' + labels[i] + '\n' + r'$=' + value + '$'
         if len(config.BASEMENT.fitkeys)>1:
-            caxes[i,i].set_title(ctitle)
+            # caxes[i,i].set_title(ctitle)
+            caxes[i,i].set_title(ctitle, fontsize=32, rotation=45, horizontalalignment='left')
             for i in range(caxes.shape[0]):
                 for j in range(caxes.shape[1]):
                     caxes[i,j].xaxis.set_label_coords(0.5, -0.5)
                     caxes[i,j].yaxis.set_label_coords(-0.5, 0.5)
+        
+                    if i==(caxes.shape[0]-1): 
+                        fmt = ScalarFormatter(useOffset=False)
+                        caxes[i,j].xaxis.set_major_formatter(fmt)
+                    if (i>0) and (j==0):
+                        fmt = ScalarFormatter(useOffset=False)
+                        caxes[i,j].yaxis.set_major_formatter(fmt)
+                        
+                    for tick in caxes[i,j].xaxis.get_major_ticks(): tick.label.set_fontsize(24) 
+                    for tick in caxes[i,j].yaxis.get_major_ticks(): tick.label.set_fontsize(24)    
         else:
             caxes.set_title(ctitle)
             caxes.xaxis.set_label_coords(0.5, -0.5)
             caxes.yaxis.set_label_coords(-0.5, 0.5)
-        
+            
             
     return fig
 
@@ -270,27 +281,27 @@ def mcmc_output(datadir):
     
     
     #::: print autocorr
-    print_autocorr(reader)
+    # print_autocorr(reader)
 
 
     #::: plot the fit
-    posterior_samples = draw_mcmc_posterior_samples(reader, Nsamples=20) #only 20 samples for plotting
-    for companion in config.BASEMENT.settings['companions_all']:
-        fig, axes = afplot(posterior_samples, companion)
-        fig.savefig( os.path.join(config.BASEMENT.outdir,'mcmc_fit_'+companion+'.pdf'), bbox_inches='tight' )
-        plt.close(fig)
+    # posterior_samples = draw_mcmc_posterior_samples(reader, Nsamples=20) #only 20 samples for plotting
+    # for companion in config.BASEMENT.settings['companions_all']:
+    #     fig, axes = afplot(posterior_samples, companion)
+    #     fig.savefig( os.path.join(config.BASEMENT.outdir,'mcmc_fit_'+companion+'.pdf'), bbox_inches='tight' )
+    #     plt.close(fig)
         
-    for companion in config.BASEMENT.settings['companions_phot']:
-        for inst in config.BASEMENT.settings['inst_phot']:
-            fig, axes = afplot_per_transit(posterior_samples, inst, companion)
-            fig.savefig( os.path.join(config.BASEMENT.outdir,'mcmc_fit_per_transit_'+inst+'_'+companion+'.pdf'), bbox_inches='tight' )
-            plt.close(fig)
+    # for companion in config.BASEMENT.settings['companions_phot']:
+    #     for inst in config.BASEMENT.settings['inst_phot']:
+    #         fig, axes = afplot_per_transit(posterior_samples, inst, companion)
+    #         fig.savefig( os.path.join(config.BASEMENT.outdir,'mcmc_fit_per_transit_'+inst+'_'+companion+'.pdf'), bbox_inches='tight' )
+    #         plt.close(fig)
     
     
     #::: plot the chains
-    fig, axes = plot_MCMC_chains(reader)
-    fig.savefig( os.path.join(config.BASEMENT.outdir,'mcmc_chains.jpg'), bbox_inches='tight' )
-    plt.close(fig)
+    # fig, axes = plot_MCMC_chains(reader)
+    # fig.savefig( os.path.join(config.BASEMENT.outdir,'mcmc_chains.jpg'), bbox_inches='tight' )
+    # plt.close(fig)
 
 
     #::: plot the corner
@@ -300,35 +311,35 @@ def mcmc_output(datadir):
 
 
     #::: save the tables
-    posterior_samples = draw_mcmc_posterior_samples(reader) #all samples
-    save_table(posterior_samples, 'mcmc')
-    save_latex_table(posterior_samples, 'mcmc')
+    # posterior_samples = draw_mcmc_posterior_samples(reader) #all samples
+    # save_table(posterior_samples, 'mcmc')
+    # save_latex_table(posterior_samples, 'mcmc')
     
     
     #::: derive values (using stellar parameters from params_star.csv)
-    if os.path.exists( os.path.join(config.BASEMENT.datadir,'params_star.csv') ):
-        deriver.derive(posterior_samples, 'mcmc')
-    else:
-        print('File "params_star.csv" not found. Cannot derive final parameters.')
+    # if os.path.exists( os.path.join(config.BASEMENT.datadir,'params_star.csv') ):
+    #     deriver.derive(posterior_samples, 'mcmc')
+    # else:
+    #     print('File "params_star.csv" not found. Cannot derive final parameters.')
     
     
     #::: make top-down orbit plot (using stellar parameters from params_star.csv)
-    if os.path.exists( os.path.join(config.BASEMENT.datadir,'params_star.csv') ):
-        params_median, params_ll, params_ul = get_params_from_samples(posterior_samples)
-        params_star = np.genfromtxt( os.path.join(config.BASEMENT.datadir,'params_star.csv'), delimiter=',', names=True, dtype=None, encoding='utf-8', comments='#' )
-        try:
-            fig, ax = plot_top_down_view(params_median, params_star)
-            fig.savefig( os.path.join(config.BASEMENT.outdir,'top_down_view.pdf'), bbox_inches='tight' )
-            plt.close(fig)        
-        except:
-            warnings.warn('Orbital plots could not be produced.')
-    else:
-        print('File "params_star.csv" not found. Cannot derive final parameters.')
+    # if os.path.exists( os.path.join(config.BASEMENT.datadir,'params_star.csv') ):
+    #     params_median, params_ll, params_ul = get_params_from_samples(posterior_samples)
+    #     params_star = np.genfromtxt( os.path.join(config.BASEMENT.datadir,'params_star.csv'), delimiter=',', names=True, dtype=None, encoding='utf-8', comments='#' )
+    #     try:
+    #         fig, ax = plot_top_down_view(params_median, params_star)
+    #         fig.savefig( os.path.join(config.BASEMENT.outdir,'top_down_view.pdf'), bbox_inches='tight' )
+    #         plt.close(fig)        
+    #     except:
+    #         warnings.warn('Orbital plots could not be produced.')
+    # else:
+    #     print('File "params_star.csv" not found. Cannot derive final parameters.')
         
         
     #::: plot TTV results (if wished for)
-    if config.BASEMENT.settings['fit_ttvs'] == True:
-        plot_ttv_results(params_median, params_ll, params_ul)
+    # if config.BASEMENT.settings['fit_ttvs'] == True:
+    #     plot_ttv_results(params_median, params_ll, params_ul)
         
         
     #::: clean up and delete the tmp file