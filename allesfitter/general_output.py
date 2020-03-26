9f80f8ecf1c81d5d5713516f402cf2c94923dc00
Tansu Daylan <tansu.daylan@gmail.com>
2020-03-26 13:19:50 -0400

    Reverted the order of * syntax

--- allesfitter/general_output.py
+++ allesfitter/general_output.py
@@ -59,7 +59,7 @@ class bcolors:
     BOLD = '\033[1m'
     UNDERLINE = '\033[4m'
     
-def logprint(typ='default', *text):
+def logprint(*text, typ='default'):
     if config.BASEMENT.settings['print_progress']:
         if typ=='default':
             print(*text)
